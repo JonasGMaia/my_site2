973ae0ecd7a32e7bbd9111394367d27be2dfa744
Jonas Maia <jonasgmaia2@gmail.com>
2026-05-12 16:52:03 -0300

    fixing admin post action

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from blog.models.post import Post
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'status', 'created_on')
     list_filter = ('status',)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
-# Register your models here.
-from .models import Post
+
